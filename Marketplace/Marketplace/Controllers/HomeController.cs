806a963550650045b97bc5a6e462073c778021a4
Artemkamiass <m.a.c2001@yandex.ru>
2024-11-27 20:37:48 +0500

    Create form(Product/Create.chtml)

--- Marketplace/Marketplace/Controllers/HomeController.cs
+++ Marketplace/Marketplace/Controllers/HomeController.cs
@@ -1,22 +1,26 @@
 using Marketplace.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace Marketplace.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //private readonly ILogger<HomeController> _logger;
+        private readonly MarketplaceContext _marketplaceContext;
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public HomeController(MarketplaceContext marketplaceContext)
         {
-            _logger = logger;
+            _marketplaceContext = marketplaceContext;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _marketplaceContext.Products.Include(p => p.Category).ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

def4f69eb5e4bf9bdc7eaf94168fe9f9cf20dc8a
Artemkamiass <m.a.c2001@yandex.ru>
2024-11-27 17:22:58 +0500

    I have done work with the frontend in the product index

--- Marketplace/Marketplace/Models/Product.cs
+++ Marketplace/Marketplace/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
+using System.Collections.Generic;
 
 namespace Marketplace.Models
 {
--- Marketplace/Marketplace/Program.cs
+++ Marketplace/Marketplace/Program.cs
@@ -1,3 +1,6 @@
+using Marketplace.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Marketplace
 {
     public class Program
@@ -6,29 +9,20 @@ namespace Marketplace
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDbContext<MarketplaceContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("MarketplaceDataBase")));
+
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.UseStaticFiles();
+
+
 
             app.Run();
         }

5d7bf5c08de8412a47bcdd9aecb3f877ea29682c
Artemkamiass <m.a.c2001@yandex.ru>
2025-01-17 21:30:56 +0500

    Update frontend

--- Marketplace/Marketplace/Controllers/HomeController.cs
+++ Marketplace/Marketplace/Controllers/HomeController.cs
@@ -2,25 +2,43 @@ using Marketplace.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 
 namespace Marketplace.Controllers
 {
     public class HomeController : Controller
     {
+        private const string _cartSessionKey = "Cart";    
         //private readonly ILogger<HomeController> _logger;
         private readonly MarketplaceContext _marketplaceContext;
         //public HomeController(ILogger<HomeController> logger)
         //{
         //    _logger = logger;
         //}
+
+        private List<CartItem> GetCart()
+        {
+            var cart = HttpContext.Session.GetString(_cartSessionKey);
+            return cart == null ? new List<CartItem>() : JsonConvert.DeserializeObject<List<CartItem>>(cart);
+        }
+
         public HomeController(MarketplaceContext marketplaceContext)
         {
             _marketplaceContext = marketplaceContext;
         }
+
         public async Task<IActionResult> Index()
         {
-            return View(await _marketplaceContext.Products.Include(p => p.Category).ToListAsync());
+            var products = await _marketplaceContext.Products.Include(p => p.Category).ToListAsync();
+            var cart = GetCart();
+            foreach (var product in products)
+            {
+                var quantity = cart.FirstOrDefault(p => p.ProductId == product.Id)?.Quantity ?? 0;
+                ViewData["ProductQuantity_" + product.Id] = quantity;
+            }
+            ViewData["QuantityItems"] = cart.Count;
+            return View(products);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
